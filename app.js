c052220e095bee1ea693b680853179d074c12707
chris  ntita <106883780+Chrisntita@users.noreply.github.com>
2024-02-13 15:58:38 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//using selectors inside the element
+// traversing the dom
+
+let btns = document.querySelectorAll(".question-btn")
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+        const question = event.currentTarget.parentElement.
+        parentElement
+        question.classList.toggle("show-text")
+
+    })
+})
